1c8dbbb28d13ba3ca590dbf4063e7fa6a3b2ee56
Philemon Forson <firstkoolaid21@gmail.com>
2023-03-05 22:51:58 +0000

    more code

--- src/components/Column/AddColumnTab/AddColumnTab.tsx
+++ src/components/Column/AddColumnTab/AddColumnTab.tsx
@@ -1,14 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { ThemeContext } from "../../../contexts/ThemeContext";
 import "./AddColumnTab.css";
 
 const AddColumnTab = ({ addColumn }: any) => {
   const { theme } = useContext(ThemeContext);
+
+  useEffect(() => {
+    console.log("theme from add column");
+    console.log(theme);
+    console.log(theme === "light");
+  }, []);
+
   return (
     <div
       className={
         "child w-[280px] min-h-full flex justify-center items-center font-jakartaBold dark:bg-[rgba(43, 44, 55, 0.25)] text-2xl mt-11 cursor-pointer text-mediumGrey " +
-        (theme === "light" && "light-column-bg")
+        (theme === "light" ? "light-column-bg" : "dark-column-bg")
       }
       onClick={addColumn}
     >
@@ -18,7 +25,7 @@ const AddColumnTab = ({ addColumn }: any) => {
           d="M7.368 12V7.344H12V4.632H7.368V0H4.656v4.632H0v2.712h4.656V12z"
         />
       </svg>
-      <div className="ml-2">New Column</div>
+      <div className="ml-2">New Columns {theme === "light"}</div>
     </div>
   );
 };
